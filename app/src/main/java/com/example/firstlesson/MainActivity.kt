b880103306643c7c80606c04e25cf1cd7b029908
Iskandar <iskandar200213@gmail.com>
2021-09-20 21:50:17 +0300

    fix bug in main activity

--- app/src/main/java/com/example/firstlesson/MainActivity.kt
+++ app/src/main/java/com/example/firstlesson/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val bigCircle = Circle("bigCircle", 20.0)
-        println(bigCircle.name + bigCircle.calculateRadius())
+        println(bigCircle.name + bigCircle.calculateArea())
 
         val bigRectangle = Rectangle("bigRectangle", 20.0)
         println(bigRectangle.name + bigRectangle.calculateSquare())
