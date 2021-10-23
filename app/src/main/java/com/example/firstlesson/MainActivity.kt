b9ccbf1a5c587e5e8f4ff60340a2665e43965fa2
Iskandar <iskandar200213@gmail.com>
2021-10-23 13:43:17 +0300

    added navController

--- app/src/main/java/com/example/firstlesson/MainActivity.kt
+++ app/src/main/java/com/example/firstlesson/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.firstlesson
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.firstlesson.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
     }
